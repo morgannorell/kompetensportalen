bd912cfef07f0438cee00ab561572278220964c3
nisr1500 <nisr1500@student.miun.se>
2016-11-03 21:45:27 +0100

    timer done

--- kompetensportalen/kompetensportalen/js/timer.js
+++ kompetensportalen/kompetensportalen/js/timer.js
@@ -1,5 +1,6 @@
 ﻿function startCountdown() {
-    var countdown = 5 * 60 * 1000;
+    document.getElementById('countdownTimer').innerHTML = "30:00";
+    var countdown = 30 * 60 * 1000;
     var timerId = setInterval(function () {
         countdown -= 1000;
         var min = Math.floor(countdown / (60 * 1000));
@@ -20,13 +21,18 @@
 };
 
 function resumeCountdown() {
+    
     var countdown = document.cookie--;
+    var premin = Math.floor(countdown / (60 * 1000));
+    var presec = Math.floor((countdown - (premin * 60 * 1000)) / 1000);
+    var preresult = ('0' + premin).slice(-2) + " : " + ('0' + presec).slice(-2);
+    document.getElementById('countdownTimer').innerHTML = preresult;
+
     var timerId = setInterval(function () {
         countdown -= 1000;
         var min = Math.floor(countdown / (60 * 1000));
         var sec = Math.floor((countdown - (min * 60 * 1000)) / 1000);
         var result = ('0' + min).slice(-2) + " : " + ('0' + sec).slice(-2);
-
 
         document.cookie = countdown;
         document.getElementById('countdownTimer').innerHTML = result;
@@ -38,5 +44,6 @@ function resumeCountdown() {
             $("#countTime").html(min + " : " + sec);
         }
     }, 1000);
+    
 }
 
