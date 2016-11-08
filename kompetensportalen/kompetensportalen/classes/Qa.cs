8a8557f76b77954aa69a4e174b7905f7940cd972
morgannorell <mnorell@live.se>
2016-11-08 21:55:42 +0100

    fdsfa

---
+++ kompetensportalen/kompetensportalen/classes/Qa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace kompetensportalen.classes
+{
+    public class Qa
+    {
+        public string Fråga { get; set; }
+        public string SvarEtt { get; set; }
+        public string SvarTvå { get; set; }
+        public string SvarTre { get; set; }
+        public string SvarFyra { get; set; }
+        public string MarkeratSvar { get; set; }
+        public string RättSvar { get; set; }
+
+    }
+}
